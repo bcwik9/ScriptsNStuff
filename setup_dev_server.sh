3d53a7b634b37ea2632993bf9f1a5827d457cfcb
Ubuntu <ubuntu@ip-172-31-33-211.ec2.internal>
2014-11-19 23:57:17 +0000

    Adding a script to set a dev server up on Amazon AWS EC2 Ubuntu instance.

---
+++ setup_dev_server.sh
@@ -0,0 +1,10 @@
+echo "Updating and upgrading"
+sudo apt-get update
+sudo apt-get upgrade
+
+echo "Installing progs"
+sudo apt-get install git
+sudo apt-get install bundler
+sudo apt-get install emacs
+sudo apt-get install curl
+
