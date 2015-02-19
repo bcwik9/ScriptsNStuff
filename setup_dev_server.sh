cc5515ea7256a81fca2c812931ac0d2fa8023efe
Ben <bcwik9@gmail.com>
2015-02-19 04:45:14 +0000

    fix script

--- setup_dev_server.sh
+++ setup_dev_server.sh
@@ -28,9 +28,8 @@ sudo tar --strip-components=1 -xzf ../rvm-stable.tar.gz
 sudo ./install --auto-dotfiles
 sudo echo "source /usr/local/rvm/scripts/rvm" >> ~/.bashrc # add to bash
 # clean up RVM install files
-cd ..
-sudo rm -rf rvm/
-sudo rm -f rvm-stable.tar.gz
+sudo rm -rf ../rvm/
+sudo rm -f ../rvm-stable.tar.gz
 
 echo "Installing Ruby"
 sudo -i source /usr/local/rvm/scripts/rvm && rvm install 2.1.3
