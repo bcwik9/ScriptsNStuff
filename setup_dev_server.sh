962acc9f07089e0cd56a3a138ad301584dabda24
Ubuntu <ubuntu@ip-172-31-32-5.ec2.internal>
2015-02-19 00:11:32 +0000

    hmm some error is still happening... add some debug output

--- setup_dev_server.sh
+++ setup_dev_server.sh
@@ -33,8 +33,9 @@ sudo ./install --auto-dotfiles
 sudo echo "source /usr/local/rvm/scripts/rvm" >> ~/.bashrc # add to bash
 # clean up RVM install files
 sudo rm -rf ../rvm/
-sudo rm ../rvm-stable.tar.gz
+sudo rm -f ../rvm-stable.tar.gz
 
 echo "Installing Ruby"
 sudo -i source /usr/local/rvm/scripts/rvm && rvm install 2.1.3
+echo "Setting default Ruby version"
 sudo -i source /usr/local/rvm/scripts/rvm && rvm use 2.1.3 --default # set default ruby
