d2c4999a7a471c2369b9ecf3bfd31e3eeb09e13b
Ubuntu <ubuntu@ip-172-31-9-209.ec2.internal>
2015-02-20 00:12:02 +0000

    cd again

--- setup_dev_server.sh
+++ setup_dev_server.sh
@@ -23,20 +23,20 @@ sudo apt-get install -y $package_list
 
 echo "Installing RVM"
 sudo curl -sSL https://github.com/wayneeseguin/rvm/tarball/stable -o rvm-stable.tar.gz
-sudo mkdir rvm
-sudo tar --strip-components=1 --directory="rvm" -xzf rvm-stable.tar.gz
-sudo ./rvm/install --auto-dotfiles
+sudo mkdir rvm && cd rvm
+sudo tar --strip-components=1 -xzf ../rvm-stable.tar.gz
+sudo ./install --auto-dotfiles
 export rmv_cmd="source /usr/local/rvm/scripts/rvm"
 $rvm_cmd
 sudo echo $rvm_cmd >> ~/.bashrc # add to bash
 # clean up RVM install files
-sudo rm -rf rvm/
-sudo rm -f rvm-stable.tar.gz
+sudo rm -rf ../rvm/
+sudo rm -f ../rvm-stable.tar.gz
 
 echo "Installing Ruby"
 sudo -i rvm install 2.1.3
 sudo -i rvm use 2.1.3 --default # set default ruby
 
-echo "ALL DONE. Remember to log out and back in again..."
+echo "*** ALL DONE ***"
 trap - 0 # clear trap so script doesn't fail at the end
 exit 0
