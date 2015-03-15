a4e226c31b65c0866a5e39817915f4782518ce6a
Ubuntu <ubuntu@ip-172-31-37-57.ec2.internal>
2015-03-15 18:27:13 +0000

    make script more versatile so it can support subdomains and various port options

--- add_nginx_servers.rb
+++ add_nginx_servers.rb
@@ -3,8 +3,7 @@ def get_nginx_server_def opt={}
   listen_port = opt[:listen] || 80
   forward_port = opt[:forward]
   passenger_app_env = opt[:passenger_app_env] || 'development'
-  server_name = opt[:server_name] || 'localhost'
-  
+  server_name = opt[:server_name] || 'localhost'  
 
   raise 'Must specify project location or port to forward to' if project_location.nil? and forward_port.nil?
   
@@ -41,6 +40,8 @@ end
 
 
 # *** MAIN ***
+USAGE = 'Specify servers in the form of <listenPort|subdomain>:<forwardPort|projectRoot>'
+
 # figure out what nginx config file we're editing
 nginx_conf = ENV['NGINX_CONF_LOCATION'] || '/opt/nginx/conf/nginx.conf'
 raise "Warning: #{nginx_conf} doesn't exist!" unless File.exists? nginx_conf
@@ -58,17 +59,31 @@ servers = ARGV
 if servers.empty? and ENV['NGINX_SERVER_LIST']
   servers = ENV['NGINX_SERVER_LIST'].split ','
 end
-raise 'No servers to add' if servers.nil? or servers.empty?
+raise "No servers to add. #{USAGE}" if servers.nil? or servers.empty?
 
 # add servers
 servers.each do |v|
-  if v =~ /\d+/
-    # port forwarding
-    temp_nginx_conf_file.push get_nginx_server_def({:forward => v})
+  server_spec = v.split ':'
+  raise "Invalid server specification. #{USAGE}" if server_spec.size != 2
+  
+  # determine if we're listening to a specific port or
+  # looking out for a subdomain on (default) port 80
+  opt = {}
+  if server_spec.first =~ /\d+/
+    opt[:listen] = server_spec.first
   else
-    # passenger support
-    temp_nginx_conf_file.push get_nginx_server_def({:project_location => v})
+    opt[:server_name] = server_spec.first
   end
+
+  # determine where the traffic is routed to
+  if server_spec.last =~ /\d+/
+    opt[:forward] = server_spec.last
+  else
+    opt[:project_location] = server_spec.last
+  end
+
+  # generate nginx server spec and add it to our temp config file
+  temp_nginx_conf_file.push get_nginx_server_def(opt)
 end
 
 # close http section
