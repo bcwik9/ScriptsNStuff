dc1579c3e0d0fd66e0738acb112cf8c793b879c5
U-spacemonkies\spacemonkey <bcwik9@gmail.com>
2015-04-06 23:54:36 -0400

    script now works properly for numbers up to (10^36)-1

--- num_to_english.rb
+++ num_to_english.rb
@@ -31,35 +31,68 @@ DOUBLE = {
   90 => 'ninety'
 }
 
-BIG = {
-  10 ** 2 => 'hundred',
-  10 ** 3 => 'thousand',
-  10 ** 6 => 'million',
-  10 ** 9 => 'billion',
-  10 ** 12 => 'trillion',
-  10 ** 15 => 'quadrillion',
-  10 ** 18 => 'quintillion',
-  10 ** 21 => 'sextillion',
-  10 ** 24 => 'septillion',
-  10 ** 27 => 'octillion',
-  10 ** 30 => 'nonillion',
-  10 ** 33 => 'decillion'
-}
+BIG = [
+  'thousand',
+  'million',
+  'billion',
+  'trillion',
+  'quadrillion',
+  'quintillion',
+  'sextillion',
+  'septillion',
+  'octillion',
+  'nonillion',
+  'decillion'
+]
 
 # given an integer, returns the english representation
 def num_to_english i
   # some simple sanity checks
   ret = ''
-  return 'zero' if i == 0
+  return ret if i.nil? # return empty string if were given nothing
+  return 'zero' if i.to_i == 0
 
-  # start ripping the number apart
-  two_digits = i % 100
+  # check to make sure we can support the english version
+  max = (10**((BIG.size+1) * 3)) - 1
+  raise 'Number too large' if max < i.to_i
+
+  power_counter = 0
+  num_arr = i.split ''
+  while !num_arr.empty?
+    current_num = num_arr.pop(3).join ''
+    current_english = get_hundred_representation current_num
+    postfix = (power_counter == 0) ? '' : BIG[power_counter-1]
+    power_counter += 1
+    next if current_english.empty?
+    current_english += " #{postfix}" unless postfix.empty?
+    ret = current_english + ' ' + ret
+  end
+  
+  return ret
+end
+
+# takes a string representation of a number up to 999
+# returns the english counterpart of the parameter
+def get_hundred_representation num
+  raise 'Invalid number specified' unless num.size <= 3
+  ret = ''
+
+  # hundred position
+  if num.size == 3
+    hundred = num[0].to_i
+    ret = "#{SINGLE[hundred]} hundred" if hundred != 0
+  end
+
+  # rightmost two digits are special
+  two_digits = num.to_i % 100
+  return ret if two_digits == 0
+  ret += ' ' unless ret.empty?
   if two_digits < DOUBLE.keys.min
-    ret = SINGLE[two_digits]
+    ret += SINGLE[two_digits]
   else
     single_digit = two_digits % 10
-    ret = DOUBLE[two_digits-single_digit]
-    ret += " " + SINGLE[single_digit] if SINGLE.has_key? single_digit
+    ret += DOUBLE[two_digits-single_digit]
+    ret += " " + SINGLE[single_digit] unless single_digit == 0
   end
 
   return ret
@@ -67,5 +100,4 @@ end
 
 # *** MAIN ***
 num = ARGV.first
-return '' if num.nil?
-puts num_to_english num.to_i
+puts num_to_english num
