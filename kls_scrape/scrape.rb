2e0aca5b776e7fc013203650032b4a13d7ce10ef
Ben Cwik <bcwik9@gmail.com>
2016-05-02 17:39:35 +0000

    capture all phone numbers AND emails

--- kls_scrape/scrape.rb
+++ kls_scrape/scrape.rb
@@ -7,8 +7,16 @@ Spidr.site(website) do |spider|
     puts "[-] #{page.url}"
     next if page.url.to_s =~ /\.js/i # skip js pages
 
-    if page.body =~ /\(*(\d{3})\)*\s*-*\.*\s*(\d{3})\s*-*\.*\s*(\d{4})/ # regex matches a phone number
-      puts "FOUND PHONE NUMBER: #{$1.to_s + $2.to_s + $3.to_s}"
+    phone_number_regex = /\(*(\d{3})\)*\s*-*\.*\s*(\d{3})\s*-*\.*\s*(\d{4})/
+    matches = page.body.scan phone_number_regex
+    matches.each do |match|
+      puts "FOUND PHONE NUMBER: #{match.join ''}"
+    end
+
+    email_regex = /(([\w+\-](\.[\w+\-])?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+)/i
+    matches = page.body.scan email_regex
+    matches.each do |match|
+      puts "FOUND EMAIL: #{match[0].to_s}"
     end
   end
 end
