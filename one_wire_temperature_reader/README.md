099d861b9660952730be5cc4fe3d7928cad550f1
Ben Cwik <bcwik9@gmail.com>
2017-01-23 01:56:35 +0000

    add 1 wire ds18b20 temperature sensor reader script

---
+++ one_wire_temperature_reader/README.md
@@ -0,0 +1,9 @@
+### 1-wire DS18B20 Temperature sensor module reader
+This is a simple ruby script that outputs the temperatures of 1-wire DS18B20 temperature sensors. Tested using Raspberry Pi 3 and [CHIP](http://www.getchip.com)
+
+
+### Tutorial
+[Tutorial](https://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing/hardware)
+
+### Wire Diagram for 
+[Wire Diagram Link](https://learn.adafruit.com/assets/3782)
---
+++ one_wire_temperature_reader/read_temp.rb
@@ -0,0 +1,19 @@
+while true
+  threads = []
+  high = -99999
+  low = 99999
+  sum = 0
+  Dir.glob('/sys/bus/w1/devices/28-*').each do |sensor_folder|
+    threads << Thread.new do
+      temp_data = File.readlines File.join(sensor_folder, 'w1_slave')
+      temperature_c = $1.to_i/1000.0 if temp_data.last =~ /t=(\d+)/
+      temperature_f = temperature_c * 9/5 + 32
+      low = temperature_f if low > temperature_f
+      high = temperature_f if high < temperature_f
+      sum += temperature_f
+      puts "sensor #{sensor_folder}: #{temperature_f}f"
+    end
+  end
+  threads.each { |t| t.join }
+  puts "num_sensors: #{threads.size}, high: #{high}f, low: #{low}f, avg: #{sum/threads.size.to_f}, diff: #{high-low}f"
+end
